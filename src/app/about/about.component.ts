13e47f4c72ed2ceb508e2415c498235070b19759
Guv <Guv@thehouseshop.com>
2020-05-01 21:21:14 +0100

    example of batch updates to firebase

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Course } from '../model/course';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { of } from 'rxjs';
 
 
 @Component({
@@ -10,10 +11,27 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
 
   ngOnInit() {
-    
+
+  }
+
+  save() {
+    const firebaseCourseRef = this.db.doc('/courses/q7O0dJJaQyZ48sLLT7we').ref;
+    const angularSecCourseRef = this.db.doc('/courses/SaVJ9mh9vqnCFrPBSbvf').ref;
+
+    const batch = this.db.firestore.batch();
+
+    batch.update(firebaseCourseRef, {titles: {description: 'Yo Firebase Course'}});
+    batch.update(angularSecCourseRef, {titles: {description: 'Yo Angular Security Course'}});
+
+    const batch$ = of(batch.commit());
+
+    batch$.subscribe();
   }
 
 }
+
+// you can update multiple documents via id by using the above methods
+// the current limit is 500 at a time
